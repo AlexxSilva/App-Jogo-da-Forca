6b02b960fad869b1a4459721100b90f1b55f9477
Alex Silva <alex@mtcsoft.com.br>
2024-06-10 20:04:02 -0300

    Adicionar arquivos de projeto.

---
+++ AppJogoDaForca/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using AppJogoDaForca.Libraries.Text;
+using AppJogoDaForca.Models;
+using AppJogoDaForca.Repositories;
+
+namespace AppJogoDaForca
+{
+    public partial class MainPage : ContentPage
+    {
+        private Word _word;
+        private int _errors = 0;
+        public MainPage()
+        {
+            InitializeComponent();
+            var repository = new WordRepositories();
+            _word = repository.GetRandomWord();
+            LblTips.Text = _word.Tips;
+            LblText.Text = new string('_', _word.Text.Length);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            ((Button)sender).IsEnabled = false;
+           string letter = ((Button)sender).Text;// cast
+           var positions = _word.Text.GetPositions(letter);
+
+            if (positions.Count == 0)
+            {
+                _errors++;
+                return;
+            }
+
+            foreach (int position in positions)
+            {
+                LblText.Text = LblText.Text.Remove(position, 1).Insert(position, letter);
+                
+            }
+        }
+    }
+
+}
---
+++ AppJogoDaForca/Repositories/WordRepositories.cs
@@ -0,0 +1,29 @@
+﻿using AppJogoDaForca.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppJogoDaForca.Repositories
+{
+    public class WordRepositories
+    {
+        private List<Word> _words;
+        public WordRepositories()
+        {
+            _words = new List<Word>();
+            _words.Add(new Word("Nome", "Maria".ToUpper()));
+            _words.Add(new Word("Vegetal", "Cenoura".ToUpper()));
+            _words.Add(new Word("Fruta", "Abacate".ToUpper()));
+            _words.Add(new Word("Tempero", "Nordestino".ToUpper()));
+            _words.Add(new Word("Tempero", "Baiano".ToUpper()));
+        }
+        public Word GetRandomWord()
+        {
+            Random rand = new Random();
+            var number = rand.Next(0, _words.Count);
+            return _words[number];
+        }
+    }
+}
