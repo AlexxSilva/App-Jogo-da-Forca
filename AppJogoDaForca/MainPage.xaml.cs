c9a938a57237a693ee2ab9c78caeb695cf769a8a
Alex Silva <alex@mtcsoft.com.br>
2024-06-16 08:13:46 -0300

    Alterações finais

--- AppJogoDaForca/MainPage.xaml.cs
+++ AppJogoDaForca/MainPage.xaml.cs
@@ -26,33 +26,73 @@ namespace AppJogoDaForca
         {
             Button button = (Button)sender;
 
-           button.IsEnabled = false;
-           string letter = button.Text;// cast
-           var positions = _word.Text.GetPositions(letter);
+            button.IsEnabled = false;
+            string letter = button.Text;// cast
+            var positions = _word.Text.GetPositions(letter);
 
             if (positions.Count == 0)
             {
-                _errors++;
-                ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
-                //a posição no element at é 1, pq o 0 é as cores e o 1 é o estilo.
-                button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Fail"] as Style;
-
-                if (_errors == 6)
-                {
-                    await DisplayAlert("Perdeu", "Você foi enforcado", "Novo Jogo");
-                    ResetScreen();
-                }
+                ErrorHandler(button);
+                await IsGameOver();
                 return;
             }
 
+            ReplaceLetter(letter, positions);
+
+            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Success"] as Style;
+
+            await HasWinner();
+        }
+
+        private void OnButtonClickResetGame(object sender, EventArgs e)
+        {
+            ResetScreen();
+        }
+
+        #region handler Success
+
+        private async Task HasWinner()
+        {
+            if (!LblText.Text.Contains("_"))
+            {
+                await DisplayAlert("Parabéns", "Você ganhou o jogo!", "Novo Jogo");
+                ResetScreen();
+            }
+        }
+
+        private void ReplaceLetter(string letter, List<int> positions)
+        {
             foreach (int position in positions)
             {
                 LblText.Text = LblText.Text.Remove(position, 1).Insert(position, letter);
-                
-            }
 
-            button.Style = App.Current.Resources.MergedDictionaries.ElementAt(1)["Success"] as Style;
+            }
         }
+
+        #endregion
+
+        #region handler Fail
+
+        private void ErrorHandler(Button button)
+        {
+            _errors++;
+            ImgMain.Source = ImageSource.FromFile($"forca{_errors + 1}.png");
+            //a posição no element at é 1, pq o 0 é as cores e o 1 é o estilo.
+            button.Style =   App.Current.Resources.MergedDictionaries.ElementAt(1)["Fail"] as Style;
+        }
+
+        private async Task IsGameOver()
+        {
+            if (_errors == 6)
+            {
+                await DisplayAlert("Perdeu", "Você foi enforcado", "Novo Jogo");
+                ResetScreen();
+            }
+        }
+
+        #endregion
+
+        #region ResetGame - Back Screento Initial State
 
         private void ResetScreen()
         {
@@ -87,8 +127,12 @@ namespace AppJogoDaForca
             foreach (Button button in horizontal.Children)
             {
                 button.IsEnabled = true;
+                button.Style = null;
             }
         }
+        #endregion
+
+       
     }
 
 }
